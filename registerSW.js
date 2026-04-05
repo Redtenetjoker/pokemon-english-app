24db69d2192654570700c9a8f56ce6116fc65de3
Redtenetjoker <mlbbeva00@hotmail.com>
2026-04-05 19:00:23 +0800

    Fix base path - resources now load correctly

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pokemon-english-app/sw.js', { scope: '/pokemon-english-app/' })})}
